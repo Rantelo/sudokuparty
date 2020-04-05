6f47bab419016e4adbb898fe4aad0da8d3ff11cf
Roberto Antelo <roberto_antelo@hotmail.com>
2020-04-04 22:55:46 -0600

    Style font sizes

--- src/components/NumberSelector.js
+++ src/components/NumberSelector.js
@@ -3,32 +3,17 @@ import React from 'react';
 const NumberSelector = ({ currentNumber, setter }) => {
   return (
     <div className="current-select">
-      <div className="current-select__first">
-        {
-          '12345'.split('').map(e => (
-            <div
-              key={`${e}-currentselect`}
-              className={`${(e === currentNumber) ? "current" : ""}`}
-              onClick={() => setter(e)}
-            >
-              {e}
-            </div>
-          ))
-        }
-      </div>
-      <div className="current-select__second">
-        {
-          '67890'.split('').map(e => (
-            <div
-              key={`${e}-currentselect`}
-              className={`${(e === currentNumber) ? "current" : ""}`}
-              onClick={() => setter(e)}
-            >
-              {e}
-            </div>
-          ))
-        }
-      </div>
+      {
+        '1234567890'.split('').map(e => (
+          <div
+            key={`${e}-currentselect`}
+            className={`${(e === currentNumber) ? "current" : ""}`}
+            onClick={() => setter(e)}
+          >
+            {e}
+          </div>
+        ))
+      }
     </div>
   )
 }
