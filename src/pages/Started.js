763db5f117e813161e0295fa313124d692e47cf3
Roberto Antelo <roberto_antelo@hotmail.com>
2020-04-09 00:00:32 -0500

    Create default clues mock

--- src/pages/Started.js
+++ src/pages/Started.js
@@ -5,12 +5,12 @@ import Board from '../components/Board';
 import NumberSelector from '../components/NumberSelector';
 
 function Started() {
+  const original = '100000000000000000000000000000000000000000000000000000000000000000000000000000001';
   const [current, setCurrent] = React.useState('1');
-  const [numbers, setNumbers] = React.useState(
-    '100000000000000000000000000000000000000000000000000000000000000000000000000000001'
-  );
+  const [numbers, setNumbers] = React.useState(original);
 
   const setCell = (idx) => {
+    console.log('clicked', idx)
     let tempMatrix = [...numbers];
     tempMatrix[idx] = (tempMatrix[idx] === current) ? 0 : current;
     setNumbers(tempMatrix.join(''));
@@ -20,6 +20,7 @@ function Started() {
     (id === "1") && <div className="page page-started">
       <div className="sudoku-party">
         <Board
+          original={original}
           numbers={numbers}
           setNumber={setCell}
         />
