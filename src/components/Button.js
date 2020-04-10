87e76562698d871428911066eba21ae1586ed737
alan-serrano <alan.serrano.pch@gmail.com>
2020-04-10 15:04:38 -0400

    Design Button Component

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Button.scss';
+
+const Button = ({children, iconUrl = '', onClick}) => {
+
+    const icon = (iconUrl !== '') && <span className='btn-icon'>
+        <img src={iconUrl} alt=''/>
+    </span>;
+
+    return (
+        <button href className="btn" onClick={onClick}>
+            <span>{children}</span>
+            {icon}
+        </button>
+    );
+}
+
+export default Button;
